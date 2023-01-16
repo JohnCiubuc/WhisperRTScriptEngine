1554d1674320d5fcac5e06567a4ef6a7d2b46acd
John Ciubuc <john.ciubuc@ttuhsc.edu>
2023-01-15 21:06:23 -0600

    basic internet search

---
+++ test_engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 15 21:00:26 2023
+
+@author: inathero
+"""
+
+
+import whisper_script_engine as WhisperScriptEngine
+
+e = WhisperScriptEngine.WhisperRT_ScriptEngine()
+
+e.processIntent('Google Frank Tedesco')
--- whisper_script_engine.py
+++ whisper_script_engine.py
@@ -5,20 +5,26 @@ Created on Sun Jan 15 20:44:45 2023
 
 @author: inathero
 """
-import urllib.request  
+import urllib.request 
+import webbrowser
 search = ['search for', 'you search', 'google']
 class WhisperRT_ScriptEngine:
     
     def __init__(self):
         print()
     def processIntent(self, text):
+        base_text = text
+        text = text.lower()
         # This is so basic, I'm so sorry
         # Search:
         for s in search:
+            print(s)
+            print(text)
             if s in text:
                 n = text.index(s)
-                textn=text[n+len(s)+1:]
-                urllib.request.urlopen("https://www.google.com/search?q="+textn)
+                textn=base_text[n+len(s)+1:]
+                print('dd')
+                webbrowser.open("https://www.google.com/search?q="+urllib.parse.quote(textn))
                 return
                 
             
